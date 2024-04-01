57561cf4491a45fa493058500c5198cf6017a7f8
Bean <qaz326978547@gmail.com>
2024-04-01 21:21:47 +0800

    [feat] 新增API 新增/取得產品

---
+++ src/app/Http/Controllers/ProductController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+class ProductController extends Controller
+{
+    //
+    public function getProducts()
+    {
+        return response()->json([
+            'data' => 'OK'
+        ],200);
+    }
+
+    public function addProduct(Request $request)
+    {
+        $data = $request->all();
+
+        $messages = [
+            'name.required' => 'Name is required',
+            'name.string' => 'Name must be a string',
+        ];
+        $validator = Validator::make($data, [
+            'name' => 'required|string',
+        ], $messages);
+        if ($validator->fails()) {
+            return response()->json([
+                'error' => $validator->errors()
+            ],400);
+        }
+        return response()->json([
+            'data' => $data
+        ],200);
+    }
+
+}
