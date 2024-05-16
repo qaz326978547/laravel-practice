4f4f5c29e03ef6387bf53de1825ed8f82ad60406
Bean <qaz326978547@gmail.com>
2024-05-16 18:04:35 +0800

    feat : 特價商品建立索引, 新增redis cache 商品資訊, 合併特價商品與商品 Controller

--- src/app/Repositories/ProductRepositoryInterface.php
+++ src/app/Repositories/ProductRepositoryInterface.php
@@ -10,6 +10,8 @@ interface ProductRepositoryInterface
     public function getAll(): Collection;
     public function getById(int $id) : ?array;
     public function create(array $data) : Product;
-    public function update(int $id, array $data) : Product;
+    public function update(int $id, array $data) : ?array;
     public function delete(int $id) : ?Product;
+    public function getOnSale() : Collection;
+    public function updateOnSale(int $id, array $data) : ?array;
 }
